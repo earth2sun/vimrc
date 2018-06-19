3ba0e8ece714a08e443aca92239b54b29c00db98
simon <simon2sun@gmail.com>
2018-06-19 10:25:43 +0800

    check before install

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-
-vimrc=$(readlink -f vimrc)
-echo "source $vimrc" >> ~/.vimrc
+Vundle=~/.vim/bundle/Vundle.vim
+test -d $Vundle || git clone https://github.com/VundleVim/Vundle.vim.git $Vundle
+line="source $(pwd)/vimrc"
+grep -q "$line" ~/.vimrc || echo "$line" >> ~/.vimrc
+vim +PluginInstall +qall
